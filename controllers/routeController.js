584d16e3b0c040ff561b23541991bd9760fe37b9
kunknown <k.working.p@gmail.com>
2019-09-02 21:28:06 -0700

    fixed static file issue. fixed 'path' variable name

--- controllers/routeController.js
+++ controllers/routeController.js
@@ -1,5 +1,6 @@
 //routerController.js
 var nodemailer = require('./nodemailer');
+var path = require('path');
 
 //export module
 module.exports = (app, mongoose) => {
@@ -48,24 +49,24 @@ app.get('/api/logout', (req, res)=>{
     });
   });
   app.get('/api/*/get(/:id)?', (req, res) => {
-    path = req.path.toString();
+    apiPath = req.path.toString();
     id = req.params.id;
-    if(path.includes('experience')){
+    if(apiPath.includes('experience')){
       mongoose.getExp(id).then((doc) => {
         res.status(200).send({result: doc});
       });
     }
-    else if(path.includes('skills')){
+    else if(apiPath.includes('skills')){
       mongoose.getSkill().then((doc) => {
         res.status(200).send({result: doc});
       });
     }
-    else if(path.includes('education')){
+    else if(apiPath.includes('education')){
       mongoose.getEdu(id).then((doc) => {
         res.status(200).send({result: doc});
       });
     }
-    else if(path.includes('projects')){
+    else if(apiPath.includes('projects')){
       mongoose.getProj(id).then((doc) => {
         res.status(200).send({result: doc});
       });
@@ -79,28 +80,28 @@ app.get('/api/logout', (req, res)=>{
 
   //edit 
   app.post('/api/*/edit/*', (req, res) => {
-    path = req.path.toString();
-    if(path.includes('home')){
+    apiPath = req.path.toString();
+    if(apiPath.includes('home')){
       mongoose.updateIntro(req.body).then(() => {
         res.status(200).send({});
       });
     }
-    else if(path.includes('experience')){
+    else if(apiPath.includes('experience')){
       mongoose.updateExp(req.body).then(() => {
         res.status(200).send({});
       });
     }
-    else if(path.includes('skills')){
+    else if(apiPath.includes('skills')){
       mongoose.updateSkill(req.body).then(() => {
         res.status(200).send({});
       });
     }
-    else if(path.includes('education')){
+    else if(apiPath.includes('education')){
       mongoose.updateEdu(req.body).then(() => {
         res.status(200).send({});
       });
     }
-    else if(path.includes('projects')){
+    else if(apiPath.includes('projects')){
       mongoose.updateProj(req.body).then(() => {
         res.status(200).send({});
       });
@@ -109,23 +110,23 @@ app.get('/api/logout', (req, res)=>{
 
   //add api
   app.post('/api/*/add', (req, res) => {
-    path = req.path.toString();
-    if(path.includes('experience')){
+    apiPath = req.path.toString();
+    if(apiPath.includes('experience')){
       mongoose.addExp(req.body).then(() =>{
         res.status(200).send({});
       });
     }
-    else if(path.includes('skill')){
+    else if(apiPath.includes('skill')){
       mongoose.addSkill(req.body).then(() =>{
         res.status(200).send({});
       });
     }
-    else if(path.includes('education')){
+    else if(apiPath.includes('education')){
       mongoose.addEdu(req.body).then(() =>{
         res.status(200).send({});
       });
     }
-    else if(path.includes('projects')){
+    else if(apiPath.includes('projects')){
       mongoose.addProj(req.body).then(() =>{
         res.status(200).send({});
       });
@@ -134,23 +135,23 @@ app.get('/api/logout', (req, res)=>{
 
   //delete api
   app.delete('/api/*/delete/:id', (req, res) => {
-    path = req.path.toString();
-    if(path.includes('experience')){
+    apiPath = req.path.toString();
+    if(apiPath.includes('experience')){
       mongoose.deleteExp(req.params.id).then(() =>{
         res.status(200).send({});
       });
     }
-    else if(path.includes('skill')){
+    else if(apiPath.includes('skill')){
       mongoose.deleteSkill(req.params.id).then(() =>{
         res.status(200).send({});
       });
     }
-    else if(path.includes('education')){
+    else if(apiPath.includes('education')){
       mongoose.deleteEdu(req.params.id).then(() =>{
         res.status(200).send({});
       });
     }
-    else if(path.includes('projects')){
+    else if(apiPath.includes('projects')){
       mongoose.deleteProj(req.params.id).then(() =>{
         res.status(200).send({});
       });
@@ -168,8 +169,8 @@ app.post('/api/contact/mail', (req, res) => {
   });
 });
 
-  //redirect all other routes
-  app.get('*', (req, res) => {
-    res.sendFile('F:/MEAN Projects/MEAN_kPatel/public/index.html');
-  });
+//redirect all other routes
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../public/index.html'));
+});
 }
